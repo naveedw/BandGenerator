536d4a01bb9ccf1077d92ba204aaeb0cf80d8ef2
Waqas Naveed <waqas.naveed@gmail.com>
2022-02-28 01:59:52 +0000

    added input functionality

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BandGenerator
+This is an example program from Python 100 days of Code by Dr. Angela
---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Welcome to the '<< Band Name Generator >>' "+"\n")
+#2. Ask the user for the city that they grew up in.
+cityName = input("Please enter city name that they grew up in : \n")
+#3. Ask the user for the name of a pet.
+petName = input("Please enter the name of a pet : \n")
+#4. Combine the name of their city and pet and show them their band name.
+combineName = cityName +" "+ petName
+print("The band name is ....." +combineName)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
